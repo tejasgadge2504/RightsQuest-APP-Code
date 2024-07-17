059845a13667271d889368904a75da8b0870f020
TEJAS GADGE <tejasgadge903@gmail.com>
2024-07-17 18:19:43 +0530

    LoginPage added

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:moralmentor/Screens/quiz.dart';
 import 'package:moralmentor/src/colors.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,27 +10,99 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true;
+
   @override
   Widget build(BuildContext context) {
-    var size=MediaQuery.of(context).size;
+    var size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Text(
-              "MORALMENTOR",
-              style: TextStyle(color: AppBarTextColor.shade600,fontWeight: FontWeight.bold,fontSize: 25),
-            ),
-            SizedBox(width: size.width*0.3,),
-            InkWell(onTap: (){
-              // Direct To Profile Page on Tap
-            },
-                child: Icon(Icons.person,color: AppBarTextColor,)),
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/LoginAvtar.png',width: 200,),
+              Text('Login to Start your Journey to become a Rights Questee',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,),),
+              SizedBox(height: 8,),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              TextField(
+                controller: _passwordController,
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle the submit button press
+                  String email = _emailController.text;
+                  String password = _passwordController.text;
+                  print('Email: $email');
+                  print('Password: $password');
+                  // Perform login action here
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const QuizPage()),
+                  );
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                child: Text('Login',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),),
+              ),
+              SizedBox(height: 16.0),
+              TextButton(
+                onPressed: () {
+                  // Handle the signup button press
+                  print('Navigating to signup page');
+                  // Add your signup navigation logic here
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
+                    ),
+                    Text(" SignUp",
+                        style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold,fontSize: 18),)
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-        backgroundColor: AppBarColor,
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 }
